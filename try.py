278184ec3a54ea1471e8a1c87a4a471d8804365c
Nikhil Patidar <nikhilpatidar021@gmail.com>
2022-09-20 19:10:14 +0530

    changes

--- try.py
+++ try.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-import time
 import pandas as pd
+import time
 
 
 
